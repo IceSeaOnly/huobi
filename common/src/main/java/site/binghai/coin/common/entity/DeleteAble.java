d8329f9514fdacef815a7316c0c8b4703697ed5d
OnlyIceSea <1041414957@qq.com>
2018-01-01 16:37:54 +0800

    MappedSuperclass

--- common/src/main/java/site/binghai/coin/common/entity/DeleteAble.java
+++ common/src/main/java/site/binghai/coin/common/entity/DeleteAble.java
@@ -2,11 +2,14 @@ package site.binghai.coin.common.entity;
 
 import site.binghai.coin.common.utils.TimeFormat;
 
+import javax.persistence.MappedSuperclass;
+
 /**
  * Created by binghai on 2018/1/1.
  *
  * @ huobi
  */
+@MappedSuperclass
 public abstract class DeleteAble {
     public long created;
     public String createdTime;
