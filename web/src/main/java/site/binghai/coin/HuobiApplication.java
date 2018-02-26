008a6b4f6feba1c1ab53c39a3a99d68d034f8bca
OnlyIceSea <1041414957@qq.com>
2018-02-27 00:15:36 +0800

    水位监控 update

--- web/src/main/java/site/binghai/coin/HuobiApplication.java
+++ web/src/main/java/site/binghai/coin/HuobiApplication.java
@@ -1,16 +1,36 @@
 package site.binghai.coin;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import site.binghai.coin.common.entity.WaterLevelMonitor;
+import site.binghai.coin.cron.A_FistfulOfDollars;
+import site.binghai.coin.cron.RefreshHistoryOrders;
+import site.binghai.coin.cron.WaterLevelMonitorRunner;
 
 @SpringBootApplication
 @EnableScheduling
 @ComponentScan("site.binghai.coin.*")
-public class HuobiApplication {
+public class HuobiApplication implements CommandLineRunner{
+
+    @Autowired
+    private WaterLevelMonitorRunner waterLevelMonitorRunner;
+    @Autowired
+    private RefreshHistoryOrders refreshHistoryOrders;
+    @Autowired
+    private A_FistfulOfDollars a_fistfulOfDollars;
 
     public static void main(String[] args) {
         SpringApplication.run(HuobiApplication.class, args);
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+        waterLevelMonitorRunner.run();
+        refreshHistoryOrders.refresh();
+        a_fistfulOfDollars.work();
     }
 }
